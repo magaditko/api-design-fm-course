ecc4b36a4f4884977e21f5cdac79ae1bd0d56307
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-15 17:02:19 +0300

    create router placeholders

---
+++ src/router.ts
@@ -0,0 +1,69 @@
+import { Router } from 'express';
+
+const router = Router();
+
+/* PRODUCT */
+
+router.get('/product', (req, res) => {
+
+})
+
+router.get('/product/:id', (req, res) => {
+  
+})
+
+router.post('/product', (req, res) => {
+  
+})
+
+router.put('/product/:id', (req, res) => {
+  
+})
+
+router.delete('/product/:id', (req, res) => {
+  
+})
+
+/* UPDATE */
+
+router.get('/update', (req, res) => {
+
+})
+
+router.get('/update/:id', (req, res) => {
+  
+})
+
+router.post('/update', (req, res) => {
+  
+})
+
+router.put('/update/:id', (req, res) => {
+  
+})
+
+router.delete('/update/:id', (req, res) => {
+  
+})
+
+/* UPDATE POINT */
+
+router.get('/updatepoint', (req, res) => {
+
+})
+
+router.get('/updatepoint/:id', (req, res) => {
+  
+})
+
+router.post('/updatepoint', (req, res) => {
+  
+})
+
+router.put('/updatepoint/:id', (req, res) => {
+  
+})
+
+router.delete('/updatepoint/:id', (req, res) => {
+  
+})
