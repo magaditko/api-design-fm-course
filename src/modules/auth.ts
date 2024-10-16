e2197ae38c7fd80d06361b1f8bc2d0e61a0c99ec
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-16 08:31:02 +0300

    Add middleware to protect the api router

--- src/modules/auth.ts
+++ src/modules/auth.ts
@@ -7,3 +7,33 @@ export const createJWT = (user) => {
   );
   return token;
 };
+
+export const protect = (req, res, next) => {
+  const bearer = req.headers.authorization;
+
+  if (!bearer) {
+    res.status(401);
+    res.json({message: 'not authorized'});
+    return;
+  }
+
+  const [, token] = bearer.split(" ");
+  if (!token) {
+    res.status(401);
+    res.json({message: 'not valid token'});
+    return;
+  }
+
+  try {
+    const user = jwt.verify(token, process.env.JWT_SECRET)
+    req.user = user;
+    console.log(user);
+    next()
+    return;
+  } catch (e) {
+    console.error(e)
+    res.status(401);
+    res.json({message: 'not valid token'});
+    return;
+  }
+}
