d1ada7dd47c3271928ef4329399963d9cc1d1f49
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-16 10:01:50 +0300

    extract the error handling to a separate middleware
    * add validation to PUT and POST product routes

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { body, validationResult } from 'express-validator';
+import { handleInputErrors } from './modules/middleware';
 
 const router = Router();
 
@@ -13,22 +14,12 @@ router.get('/product/:id', (req, res) => {
 
 })
 
-router.post('/product', body('name').isString() ,(req, res) => {
-  const errors = validationResult(req);
-
-  if (!errors.isEmpty()) {
-    res.status(400)
-    res.json({errors: errors.array()});
-  }
+router.post('/product', body('name').isString(), handleInputErrors ,(req, res) => {
+  
 })
 
-router.put('/product/:id', body("name").isString(), (req, res) => {
-  const errors = validationResult(req);
-
-  if(!errors.isEmpty()) {
-    res.status(400)
-    res.json({errors: errors.array()})
-  }
+router.put('/product/:id', body("name").isString(), handleInputErrors, (req, res) => {
+  
 })
 
 router.delete('/product/:id', (req, res) => {
