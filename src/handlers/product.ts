b9c401d935861a70df856c7636df0ad935cb51cd
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-16 10:48:59 +0300

    add handlers for all operations on products and add unique migration

--- src/handlers/product.ts
+++ src/handlers/product.ts
@@ -29,14 +29,45 @@ export const getOneProduct = async (req, res) => {
   res.json({ data: product })
 }
 
+// Create a product
 export const createNewProduct = async (req, res) => {
+  const product = await prisma.product.create({
+    data: {
+      name: req.body.name,
+      belongsToId: req.user.id
+    }
+  })
 
+  res.json({data: product})
 }
 
+// Update product
 export const updateProduct = async (req, res) => {
-  
+  const updated = await prisma.product.update({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id
+      }
+    }, 
+    data: {
+      name: req.body.name
+    }
+  })
+
+  res.json({data: updated})
 }
 
+// Deleta
 export const deleteProduct = async (req, res) => {
+  const deleted = await prisma.product.delete({
+    where: {
+      id_belongsToId: {
+        id: req.params.id,
+        belongsToId: req.user.id
+      }
+    }
+  })
   
+  res.json({data: deleted})
 }
