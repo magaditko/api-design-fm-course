a2af6f212288657ff6cfd4e865d5337b77b5bc47
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-23 16:32:01 +0300

    Basic sync and async error handlers

--- src/server.ts
+++ src/server.ts
@@ -12,15 +12,27 @@ app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
-app.get('/', (req, res) => {
-  console.log('hello expres')
-  res.status(200);
-  res.json({message: 'hello'}) 
+app.get('/', (req, res, next) => {
+  // console.log('hello expres')
+  // res.status(200);
+  // res.json({message: 'hello'}) 
+
+  // if we pass something to next it is treated like an error
+  // we have to handle async errors and tell Express 
+  setTimeout(() => {
+    next(new Error('some error message' ))
+  }, 10)
 })
 
 app.use('/api', protect, router)
 
 app.post('/user', createNewUser);
 app.post('/signin', signin)
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.json({message: 'oops! there was an error!'})
+
+})
 
 export default app;
