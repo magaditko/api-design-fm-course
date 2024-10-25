8a11bf30db4e49d65a9c474bce8582b40f9e5fa5
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-25 14:56:39 +0300

    Add config for environment and stage

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as dotenv from 'dotenv';
+import config from './config'
 dotenv.config()
 
 import app from './server'
 
-app.listen(3001, () => {
-  console.log('app listen on localhost:3001')
+app.listen(config.port, () => {
+  console.log(`app listen on localhost:${config.port}`)
 })
