a572dd6d7070ddfeb5c0d6384bb498ff506df80d
Margarita Petrova <margarita.hr.petrova@gmail.com>
2024-10-28 09:19:42 +0200

    Add simple test for creating new user

--- src/handlers/__tests__/user.test.ts
+++ src/handlers/__tests__/user.test.ts
@@ -1,5 +1,23 @@
+import * as user from '../user'
 describe('user handler', () => {
-  it('should do something when something happens', () => {
-    expect(1).toBe(1);
+  beforeEach(() => {
+    // or inside one of the other hooks make sure
+    // that we reset the database and our tests
+    // are not dependant
+
+  })
+  
+  it('should create new user', async () => {
+    const res = {
+      json({token}) {
+        expect(token).toBeTruthy();
+      },
+    };
+    const req = {body: {username: 'hello6', password: 'hi'}}
+    const spyRes = jest.spyOn(res, 'json')
+
+    await user.createNewUser(req, res, () => {}).then(() => {
+      expect(spyRes).toHaveBeenCalledTimes(1)
+    })
   })
 })
